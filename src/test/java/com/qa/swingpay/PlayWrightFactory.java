c22bc5bf097bbc17a5fa6b9b6cfb9b3d60cdf4f8
Prasuna <Prasuna.annapureddy@clientservertech.com>
2025-08-28 20:05:55 -0600

    Some changes in Screenshot code

--- src/test/java/com/qa/swingpay/PlayWrightFactory.java
+++ src/test/java/com/qa/swingpay/PlayWrightFactory.java
@@ -116,7 +116,7 @@ public class PlayWrightFactory {
 		//getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
 		byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
 		String base64Path = Base64.getEncoder().encodeToString(buffer);
-		return path;
+		return base64Path;
 	}
 
 }
