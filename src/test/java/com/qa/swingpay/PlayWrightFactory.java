c1c15055a382a1408d090f3e3f1e850a76909a65
Prasuna <Prasuna.annapureddy@clientservertech.com>
2025-08-28 19:58:20 -0600

    Adding Jenkinsfile

--- src/test/java/com/qa/swingpay/PlayWrightFactory.java
+++ src/test/java/com/qa/swingpay/PlayWrightFactory.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Properties;
 
 import com.microsoft.playwright.Browser;
@@ -112,7 +113,9 @@ public class PlayWrightFactory {
 	public static String takeScreenshot() {
 		String path = System.getProperty("user.dir")+"/screenshot/"+System.currentTimeMillis() + ".png";
 		
-		getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		//getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions().setPath(Paths.get(path)).setFullPage(true));
+		String base64Path = Base64.getEncoder().encodeToString(buffer);
 		return path;
 	}
 
